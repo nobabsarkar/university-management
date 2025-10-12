8041cd649e7f605bb37b602a65bf64b0d7cb0ea3
Nobab Sarkar <108366419+nobabsarkar@users.noreply.github.com>
2025-10-13 01:12:09 +0600

    create middlewere of the route

--- src/app/utils/catchAsync.ts
+++ src/app/utils/catchAsync.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express';
 
+// this is higher order function. controller don't have need try catch. catchAsync is enough
 const catchAsync = (fn: RequestHandler) => {
   return (req: Request, res: Response, next: NextFunction) => {
     Promise.resolve(fn(req, res, next)).catch((err) => next(err));
