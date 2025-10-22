d86f4c88204aa2ad9b56bca2f2ef1310a0ee8f8d
Nobab Sarkar <108366419+nobabsarkar@users.noreply.github.com>
2025-10-23 01:06:30 +0600

    create transaction rollback

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -7,6 +7,7 @@ import { Student } from '../student/student.model';
 import { TUser } from './user.interface';
 import { User } from './user.model';
 import { generatedStudentId } from './user.utils';
+import mongoose from 'mongoose';
 
 const createStudentIntoDB = async (password: string, payload: TStudent) => {
   //create a user object
@@ -30,20 +31,41 @@ const createStudentIntoDB = async (password: string, payload: TStudent) => {
     );
   }
 
-  // set manually generated id
-  userData.id = await generatedStudentId(admissionSemester);
+  const session = await mongoose.startSession();
 
-  // create a user
-  const newUser = await User.create(userData); // built in static method
+  try {
+    session.startTransaction();
 
-  // create a student
-  if (Object.keys(newUser).length) {
+    // set manually generated id
+    userData.id = await generatedStudentId(admissionSemester);
+
+    // create a user (transaction-1)
+    const newUser = await User.create([userData], { session }); // array
+
+    // create a student
+    if (!newUser.length) {
+      throw new AppError(StatusCodes.BAD_REQUEST, 'Failed to create user');
+    }
+
     // set id , as user
-    payload.id = newUser.id;
-    payload.user = newUser._id; // reference _id
+    payload.id = newUser[0].id;
+    payload.user = newUser[0]._id; // reference _id
 
-    const newStudent = await Student.create(payload);
+    // create a student (transaction-2)
+    const newStudent = await Student.create([payload], { session });
+
+    if (!newStudent.length) {
+      throw new AppError(StatusCodes.BAD_REQUEST, 'Failed to create student');
+    }
+
+    await session.commitTransaction();
+    await session.endSession();
+
     return newStudent;
+  } catch (err) {
+    await session.abortTransaction();
+    await session.endSession;
+    console.log(err);
   }
 };
 
