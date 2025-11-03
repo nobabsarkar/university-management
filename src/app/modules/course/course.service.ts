d7b6142ff30c2cc477b903e557af8bcec0a9d5e3
Nobab Sarkar <108366419+nobabsarkar@users.noreply.github.com>
2025-11-04 00:04:59 +0600

    create course service

---
+++ src/app/modules/course/course.service.ts
@@ -0,0 +1,33 @@
+import { Course } from './course.model';
+
+const createCourseIntoDB = async () => {
+  const result = await Course.create();
+  return result;
+};
+
+const getAllCoursesFromDB = async () => {
+  const result = await Course.find();
+  return result;
+};
+
+const getSingleCourseFromDB = async (id: string) => {
+  const result = await Course.findById(id);
+  return result;
+};
+
+const deleteCourseIntoDB = async (id: string) => {
+  const result = await Course.findByIdAndUpdate(
+    id,
+    { isDeleted: true },
+    { new: true },
+  );
+
+  return result;
+};
+
+export const CourseServices = {
+  createCourseIntoDB,
+  getAllCoursesFromDB,
+  getSingleCourseFromDB,
+  deleteCourseIntoDB,
+};
