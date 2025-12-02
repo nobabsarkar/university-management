c715786800271a068824f75e5660079844d41daf
Nobab Sarkar <108366419+nobabsarkar@users.noreply.github.com>
2025-12-03 00:31:19 +0600

    working jwt token

---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from 'express';
+import catchAsync from '../utils/catchAsync';
+import AppError from '../errors/AppError';
+import { StatusCodes } from 'http-status-codes';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import config from '../config';
+
+const auth = () => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    // if the token is sent from the client
+    const token = req.headers.authorization;
+    if (!token) {
+      throw new AppError(StatusCodes.UNAUTHORIZED, 'You are not authorized');
+    }
+
+    // check if the token is valid
+    jwt.verify(
+      token,
+      config.jwt_access_secret as string,
+      function (err, decoded) {
+        if (err) {
+          throw new AppError(
+            StatusCodes.UNAUTHORIZED,
+            'You are not authorized',
+          );
+        }
+
+        req.user = decoded as JwtPayload;
+        next();
+      },
+    );
+  });
+};
+
+export default auth;
--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -1,18 +1,15 @@
 import { NextFunction, Request, Response } from 'express';
 import { ZodObject } from 'zod';
+import catchAsync from '../utils/catchAsync';
 
 const validateRequest = (schema: ZodObject) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      await schema.parseAsync({
-        body: req.body,
-      });
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    await schema.parseAsync({
+      body: req.body,
+    });
 
-      next();
-    } catch (err) {
-      next(err);
-    }
-  };
+    next();
+  });
 };
 
 export default validateRequest;
