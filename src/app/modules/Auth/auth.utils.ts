d2e5df7f2ee90a47644a1acd7cd4c5ff3bf3fd3e
Nobab Sarkar <108366419+nobabsarkar@users.noreply.github.com>
2025-12-06 22:12:54 +0600

    comment code

--- src/app/modules/Auth/auth.utils.ts
+++ src/app/modules/Auth/auth.utils.ts
@@ -1,11 +1,26 @@
-import jwt from 'jsonwebtoken';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// import jwt from 'jsonwebtoken';
+
+// export const createToken = (
+//   jwtPayload: { userId: string; role: string },
+//   secret: string,
+//   expiresIn: string,
+// ) => {
+//   return jwt.sign(jwtPayload, secret, {
+//     expiresIn,
+//   });
+// };
+
+import jwt, { SignOptions } from 'jsonwebtoken';
 
 export const createToken = (
   jwtPayload: { userId: string; role: string },
   secret: string,
   expiresIn: string,
 ) => {
-  return jwt.sign(jwtPayload, secret, {
-    expiresIn,
-  });
+  const options: SignOptions = {
+    expiresIn: expiresIn as any,
+  };
+
+  return jwt.sign(jwtPayload, secret, options);
 };
